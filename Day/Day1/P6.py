05c9733bfb7d3ecbe94b2487bb80cba281586894
2025cschaithrarb-cmd <2025cs_chaithrar_b@nie.ac.in>
2026-09-21 15:01:06 +0530

    fixed the format

--- Day/Day1/P6.py
+++ Day/Day1/P6.py
@@ -1,5 +1,5 @@
 #Dictionary is a collection of key-value pairs. It is unordered, mutable, and indexed.
-#  In Python, dictionaries are defined using curly braces {} and colons : to separate keys and values.
+#In Python, dictionaries are defined using curly braces {} and colons : to separate keys and values.
 person = {
     "name": "Bipin",
     "age": 25,
