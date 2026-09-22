0ae5a0e73893ebdbba389d7d843914180c4e24da
2025cschaithrarb-cmd <2025cs_chaithrar_b@nie.ac.in>
2026-09-22 12:40:02 +0530

    Basics of FastAPI with get request

---
+++ Day/Day1/FastAPIPractice/main.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI
+app = FastAPI()
+#fastapi is a library or a module and FastAPI is a class in that library. 
+#We are creating an instance of the FastAPI class and assigning it to the variable app.
