6621da0f2f8f44cff69b4f46f4997b78ba770988
2025cschaithrarb-cmd <2025cs_chaithrar_b@nie.ac.in>
2026-09-22 15:19:42 +0530

    pydantic model

--- Day/Day 2/FastAPIPractice/main.py
+++ Day/Day 2/FastAPIPractice/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+
+from pydantic import BaseModel
+
 app = FastAPI()
 @app.get("/")
 
@@ -15,14 +18,27 @@ def health():
 def create():
     return {"message": "Created"}
 
+
  #Path parameters
 
 @app.get("/students/{usn}")
-def get result(usn):
+def get_result(usn):
     return {"Result": "Distinction", "usn": usn}
+
 
 #Path parameters with type hint
 
 @app.get("/candidates/{rollno}")
 def get_candidate(rollno: int):
      return {"Result": "Distinction", "rollno": rollno,"type": str(type(rollno))}
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    in_stock: bool = True
+
+@app.post("/items")
+def create_item(item: Item):
+    return {"received": item, "total_price": item.price * 1.18}
+
