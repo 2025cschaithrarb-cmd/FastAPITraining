1f98eda85fe6f2228d1588cab53d076b23af7dea
2025cschaithrarb-cmd <2025cs_chaithrar_b@nie.ac.in>
2026-09-23 13:11:08 +0530

    cp:2

--- Day/Day3/CapstoneProject/app/database.py
+++ Day/Day3/CapstoneProject/app/database.py
@@ -5,6 +5,7 @@ from app.config import settings
 #MongoClient manages pool of connections to MongoDBserver 
 client : MongoClient = MongoClient(settings.MONGO_URI)
 database : Database = client[settings.MONGO_DB_NAME]
+
 #Sends a ping command to the MongoDB  to confirm that the connection is alive
 def ping_database() -> bool:
     try:
